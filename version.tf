f1563985f09d62e9195e0226b48bd989180306cc
mgazuredevop <107905320+mgazuredevop@users.noreply.github.com>
2022-09-24 03:17:49 +0530

    Add files via upload

---
+++ version.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_version = "1.2.7"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>3.0.0"
+        }
+      
+    }
+}
+
+provider "aws" {    
+    profile = "default"
+    region = "us-east-1"
+}
